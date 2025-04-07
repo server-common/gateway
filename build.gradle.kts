5bc95a665be3c5461bf1d99bc446320575101410
seoeungi <sjlim02255@gmail.com>
2025-04-07 22:28:44 +0900

    Init spring boot

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    val kotlinVersion = "1.9.25"
+
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+
+    id("com.google.cloud.tools.jib") version "3.4.4"
+}
+
+group = "seg.work"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.projectlombok:lombok")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+val springCloudVersion = "2024.0.1"
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+jib {
+    from {
+        image = "amazoncorretto:17-alpine-jdk"
+    }
+    to {
+        image = System.getenv("JIB_IMAGE")
+        tags = setOf("latest")
+        auth {
+            username = System.getenv("TOKEN_USER")
+            password = System.getenv("TOKEN_PWD")
+        }
+    }
+    container {
+        jvmFlags = listOf(
+            "-Dspring.security.user.name=" + System.getenv("EUREKA_USER_NAME"),
+            "-Dspring.security.user.password=" + System.getenv("EUREKA_USER_PWD"),
+            "-Dspring.jwt.secret=" + System.getenv("JWT_ENC_PWD"),
+
+            "-Djasypt.encryptor.password=\${ENC_PWD}",
+        )
+    }
+}
